00c59db91282a4400a3950d208e1f9d114374b1a
Mastermindddddd <malusicephas17@gmail.com>
2025-03-07 16:18:08 +0200

    ...

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -18,7 +18,13 @@ const Hero = () => {
       <div className="absolute inset-0 flex items-center justify-start px-4 sm:px-8 md:px-16">
         {/* Dark Oval Shape */}
         <div className="relative">
-          <div className="absolute -top-[500px] -left-20 sm:-left-40 w-[90vw] sm:w-[800px] h-[90vh] sm:h-[1260px] bg-black bg-opacity-60 rounded-[50%] transform rotate-[-6deg]"></div>
+          {/* Dark Oval Shape */}
+        <div className="absolute -top-[500px] -left-20 sm:-left-40 
+          w-[100vw] sm:w-[800px] 
+          h-[110vh] sm:h-[1260px] 
+          bg-black bg-opacity-60 rounded-[50%] transform rotate-[-6deg]">
+        </div>
+
 
           {/* Text Content */}
           <div className="relative max-w-md sm:max-w-lg text-white p-6 sm:p-10 z-10">
@@ -26,7 +32,7 @@ const Hero = () => {
               Your strategic partner in navigating the complexities of today’s business landscape to ensure future readiness.
             </h1>
             <Link to="/contact-us">
-              <button className="mt-4 sm:mt-6 bg-teal-700 text-white px-4 sm:px-6 py-2 sm:py-3 rounded-full hover:bg-teal-600 w-full sm:w-auto">
+              <button className="mt-4 sm:mt-6 bg-teal-700 text-white px-20 sm:px-20 lg:px-40 md:px-40 py-2 sm:py-3 rounded-full hover:bg-teal-600 sm:w-auto">
                 CONTACT US
               </button>
             </Link>
